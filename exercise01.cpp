973aa55e9457647fe748114e5cda9d10876c5cf5
Aaathif <99630080+Aaathif@users.noreply.github.com>
2022-02-22 17:51:57 +0000

    i check again

--- exercise01.cpp
+++ exercise01.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
+ 
  cout << "No"  <<  setw(20) << "Name" <<  setw(10) << "Marks" << endl;
  for (int r = 0; r < 5; r++) {
      cout <<  r+1 <<
--- exercise03.cpp
+++ exercise03.cpp
@@ -10,15 +10,15 @@ struct Box {
   int height;
   int width;
   int lenght;
-}
+};
 // Do not change the main function
 int main() {
     // 2. Create a variable called box1 of the Box structure type
-    struct Box box1;
+    Box box1;
     // int box1Height, box1Width, box1Length;
-    int height, width, lenght;
+    
     // 3. Create a variable called box2 of the Box structure type
-    struct Box box2;
+    Box box2;
     // int box2Height, box2Width, box2Length;
     
 
@@ -33,7 +33,7 @@ int main() {
     cin >> box1.lenght;
     
     cout << "Enter Box 2 Height : ";
-    cin >> box2.heigh;
+    cin >> box2.height;
     cout << "Enter Box 2 Width : ";
     cin >> box2.width;
     cout << "Enter Box 2 Length : ";
@@ -41,7 +41,7 @@ int main() {
     
     // 5. Replace the coding below to pass the Box type structure
     totalVolume = volume(box1.height, box1.width, box1.lenght)
-             + volume(box2.heigh, box2.width, box2.lenght);
+             + volume(box2.height, box2.width, box2.lenght);
     
     cout << "Volume of Box is " << totalVolume << endl;
     
